8e6f126e47159f17cbea6fad83e80222757e6298
NDrick03 <hendrik1506@gmail.com>
2021-10-18 23:01:38 +0200

    Initial commit

---
+++ src/de/medieninformatik/prog3/Main.java
@@ -0,0 +1,17 @@
+package de.medieninformatik.prog3;
+
+/**
+ * 1. mit awt Kreise zeichnen
+ * 2. Version mit Quadraten
+ * 3. S: Hintergrundfarben-Menü  H: geclickte Koordinate anzeigen
+ * 4. zusammenführen
+ * 5. S: Distanz berechnen u ausgeben   H: Linie zwischen Koordinaten
+ * 6. zusammenführen
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
