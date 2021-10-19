c1832ae025e705664566bde766a2311f18c46618
sebharz <sbuergel@web.de>
2021-10-19 23:50:04 +0200

    Version 1.2.1

--- src/de/medieninformatik/prog3/MainFrame.java
+++ src/de/medieninformatik/prog3/MainFrame.java
@@ -11,7 +11,7 @@ public class MainFrame extends Frame {
     private static PaintArea pa = new PaintArea();
 
     public MainFrame(MainFrameController mfc)  {
-        setTitle("Metrik Version 1.2");
+        setTitle("Metrik Version 1.2.1");
         setSize(400, 300);
 
         initializeMenu();
